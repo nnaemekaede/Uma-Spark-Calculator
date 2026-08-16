48f1fa1c32792bf101a51d23ebd4f6f630f15258
Nem <95504420+nnaemekaede@users.noreply.github.com>
2026-08-16 18:38:00 -0400

    implement computeSparkChance with correct multi-source probability combination

--- src/spark.ts
+++ src/spark.ts
@@ -1,7 +1,7 @@
 type SparkType = 'blue' | 'pink' | 'green' | 'raceWhite' | 'otherWhite';
 
 interface Spark {
-    name: string 
+    sparkName: string 
     type: SparkType;
     starLevel: 1 | 2 | 3;
 }
@@ -15,4 +15,37 @@ interface Parent {
 interface InheritanceTarget {
     sparkName: string
     acceptedTypes: SparkType[]
+}
+
+function computeSparkChance(target: InheritanceTarget, parents: Parent[]): number {
+    let totalChance = 0
+    for (const parent of parents ) {
+        let sparkMissRate = 0
+        for (const spark of parent.sparks) {
+            if (target.acceptedTypes.includes(spark.type) && target.sparkName == spark.sparkName){
+                let baseRate = 0
+                if (spark.type == 'blue'){
+                    baseRate = (.70 + .10 * (spark.starLevel - 1))   
+                } else if (spark.type == 'pink') {
+                    baseRate = (.01 + .02 * (spark.starLevel - 1))
+                } else if (spark.type == 'green') {
+                    baseRate = (.05 + .05 * (spark.starLevel - 1))
+                } else if (spark.type == 'raceWhite') {
+                    baseRate = (.01 + .01 * (spark.starLevel - 1))
+                } else if (spark.type == 'otherWhite') {
+                    baseRate = (.03 + .03 * (spark.starLevel - 1))
+                }
+                let parentSparkOdds = baseRate * (1 + parent.affinity/100) 
+                if (parent.isGrandparent) {
+                    parentSparkOdds = parentSparkOdds / 2
+                }
+                if (sparkMissRate == 0) {
+                 sparkMissRate = (1 - parentSparkOdds)
+                } else {
+                 sparkMissRate *= (1 - parentSparkOdds)
+                }  
+            }
+        }
+    }
+    return totalChance
 }
